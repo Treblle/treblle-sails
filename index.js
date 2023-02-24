12ebdee829358cbb0c9597eb5201341eff503862
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-02-24 13:32:11 +0100

    feat: implement Treblle for Sails

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
+const { sendPayloadToTreblle } = require('@treblle/utils')
+const treblleMiddleware = require('./private/treblle-middleware')
+
 /**
  * treblle hook
  *
  * @description :: A hook definition.  Extends Sails by adding shadow routes, implicit actions, and/or initialization logic.
  * @docs        :: https://sailsjs.com/docs/concepts/extending-sails/hooks
  */
-
- module.exports = function defineTreblleHook(sails) {
+module.exports = function defineTreblleHook(sails) {
   return {
     defaults: {
       treblle: {
         apiKey: sails.config.treblle?.apiKey || process.env.TREBLLE_API_KEY,
         projectId:
           sails.config.treblle?.projectId || process.env.TREBLLE_PROJECT_ID,
+        routesToMonitor: [
+          'GET r|^((?![^?]*\\/[^?\\/]+\\.[^?\\/]+(\\?.*)?).)*$|',
+          // (^^Leave out assets)
+          'POST /*',
+          'PATCH /*',
+          'PUT /*',
+          'DELETE /*',
+        ],
+        additionalFieldsToMask: [],
       },
     },
     /**
      * Runs when this Sails app loads/lifts.
      */
     initialize: async function () {
-      sails.log.info('Initializing custom hook (`treblle`)')
-      const { useTreblle } = require('treblle')
-      sails.after('hook:http:loaded', () => {
-        useTreblle(sails.hooks.http.app, sails.config.treblle)
+      sails.log.info(
+        'Initializing custom hook (`treblle`). Requests to monitored routes will be sent to Treblle...'
+      )
+      // Listen for when the router in Sails says it's time to bind "before" shadow routes:
+      sails.on('router:before', function routerBefore() {
+        sails.config.treblle.routesToMonitor.forEach(function iterator(
+          routeAddress
+        ) {
+          sails.router.bind(routeAddress, treblleMiddleware)
+        })
       })
     },
   }
---
+++ private/treblle-middleware.js
@@ -0,0 +1,108 @@
+const {
+  generateFieldsToMask,
+  maskSensitiveValues,
+  generateTrebllePayload,
+  getRequestDuration,
+} = require('@treblle/utils')
+
+const { version: sdkVersion } = require('../package.json')
+
+module.exports = function treblleMiddleware(req, res, next) {
+  // Track when this request was received.
+  const requestStartTime = process.hrtime()
+
+  const sails = req._sails
+
+  // Intercept response body
+  const originalSend = res.send
+  res.send = function sendOverWrite(body) {
+    originalSend.call(this, body)
+    this._treblleResponsebody = body
+  }
+
+  // Now bind a one-time listener that will fire when the request is finished.
+  res.on('finish', function onceFinish() {
+    let errors = []
+    const body = req.body || {}
+    const query = req.query || {}
+    const requestPayload = { ...body, ...query }
+
+    const fieldsToMask = generateFieldsToMask(
+      sails.config.treblle.additionalFieldsToMask
+    )
+    const maskedRequestPayload = maskSensitiveValues(
+      requestPayload,
+      fieldsToMask
+    )
+
+    const protocol = `${req.protocol}/${req.httpVersion}`
+
+    let originalResponseBody = res._treblleResponsebody
+    let maskedResponseBody
+    try {
+      if (Buffer.isBuffer(res.payload)) {
+        originalResponseBody = originalResponseBody.toString('utf8')
+      }
+      if (typeof originalResponseBody === 'string') {
+        let parsedResponseBody = JSON.parse(originalResponseBody)
+        maskedResponseBody = maskSensitiveValues(
+          parsedResponseBody,
+          fieldsToMask
+        )
+      } else if (typeof originalResponseBody === 'object') {
+        maskedResponseBody = maskSensitiveValues(
+          originalResponseBody,
+          fieldsToMask
+        )
+      }
+    } catch (error) {
+      // if we can't parse the body we'll leave it empty and set an error
+      errors.push({
+        source: 'onShutdown',
+        type: 'INVALID_JSON',
+        message: 'Invalid JSON format',
+        file: null,
+        line: null,
+      })
+    }
+
+    // Treblle Payload
+    const trebllePayload = generateTrebllePayload(
+      {
+        api_key: sails.config.treblle.apiKey,
+        project_id: sails.config.treblle.projectId,
+        sdk: 'sails',
+        version: sdkVersion,
+      },
+      {
+        server: {
+          protocol,
+        },
+        request: {
+          ip: req.ip,
+          url: `${req.protocol}://${req.headers['host']}${req.url}`,
+          user_agent: req.headers['user-agent'],
+          method: req.method,
+          headers: maskSensitiveValues(req.headers, fieldsToMask),
+          body: maskedRequestPayload,
+        },
+        response: {
+          headers: maskSensitiveValues(res.getHeaders(), fieldsToMask),
+          code: res.statusCode,
+          size: res.get('content-length'),
+          load_time: getRequestDuration(requestStartTime),
+          body: maskedResponseBody,
+        },
+        errors,
+      }
+    )
+
+    try {
+      sendPayloadToTreblle(trebllePayload, sails.config.treblle.apiKey)
+    } catch (error) {
+      sails.log(error)
+    }
+  })
+
+  return next()
+}
